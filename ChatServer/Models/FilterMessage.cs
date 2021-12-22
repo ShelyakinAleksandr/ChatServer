949d3762768085d7e92441cc38eb0f9bf3359d6e
Brodyaga <Brodyaga@PC1>
2021-12-22 10:38:45 +0300

    Add readme

--- ChatServer/Models/FilterMessage.cs
+++ ChatServer/Models/FilterMessage.cs
@@ -5,11 +5,23 @@ using System.Threading.Tasks;
 
 namespace ChatServer.Models
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class FilterMessage
     {
+        /// <summary>
+        /// Нужна ли сортировка по дате
+        /// </summary>
         public bool Filter { get; set; }
+        /// <summary>
+        /// Дата начала поиска
+        /// </summary>
         public DateTime DateStart { get; set; }
 
+        /// <summary>
+        /// Дата завершения поиска
+        /// </summary>
         public DateTime DateEnd { get; set; }
     }
 }
--- ChatServer/Models/Message.cs
+++ ChatServer/Models/Message.cs
@@ -5,10 +5,22 @@ using System.Threading.Tasks;
 
 namespace ChatServer.Models
 {
+    /// <summary>
+    /// Класс модели принятого сообщения
+    /// </summary>
     public class Message
     {
+        /// <summary>
+        /// Время отправки
+        /// </summary>
         public DateTime DateTimeMessege { get; set; }
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
         public string NameUser { get; set; }
+        /// <summary>
+        /// Текст сообщения
+        /// </summary>
         public string Messeges { get; set; }
     }
 }
